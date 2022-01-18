9a919adfe259f3e2094bf0ab8dd5623ef7d40f3d
Артем Шевелев <artemsevelev@Artems-MacBook-Pro.local>
2022-01-18 12:50:42 +0200

    Done

---
+++ src/components/Banner/Banner.js
@@ -0,0 +1,59 @@
+import { Container, makeStyles, Typography } from "@material-ui/core";
+
+const useStyles = makeStyles((theme) => ({
+  bannerContent: {
+    height: 400,
+    display: "flex",
+    flexDirection: "column",
+    paddingTop: 25,
+    justifyContent: "space-around",
+  },
+  tagline: {
+    display: "flex",
+    height: "40%",
+    flexDirection: "column",
+    justifyContent: "center",
+    textAlign: "center",
+  },
+  carousel: {
+    height: "50%",
+    display: "flex",
+    alignItems: "center",
+  },
+}));
+
+function Banner() {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.banner}>
+      <Container className={classes.bannerContent}>
+        <div className={classes.tagline}>
+          <Typography
+            variant="h2"
+            style={{
+              fontWeight: "bold",
+              color: "lightgreen",
+              marginBottom: 15,
+              fontFamily: "Montserrat",
+            }}
+          >
+            Crypto ARTcodeSH
+          </Typography>
+          <Typography
+            variant="subtitle2"
+            style={{
+              color: "darkgrey",
+              textTransform: "capitalize",
+              fontFamily: "Montserrat",
+            }}
+          >
+            propdo home assignment
+          </Typography>
+        </div>
+      </Container>
+    </div>
+  );
+}
+
+export default Banner;
